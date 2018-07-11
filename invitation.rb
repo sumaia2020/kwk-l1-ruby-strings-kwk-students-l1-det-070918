b3af63d335d43b7ff84b2bf77d269c2b02089cb2
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 02:18:24 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,12 @@
-# Code your prompts here!
 
-# Try starting out with puts'ing a string.
+  puts "Hi, you've been invited to a party! What is your name?"
+  gets
+  party_name = "Best Halloween Party Ever"
+  date = "October 31"
+  time = "6pm"
+  host_name = "Harry Potter"
+  guest_name = "Hermoine"
+  puts "Dear #{guest_name},
+You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
+Sincerely,
+#{host_name}"
---
+++ k.rb
@@ -0,0 +1,10 @@
+def name 
+  puts "Dear Hermione,
+ 
+You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+ 
+Sincerely,
+ 
+Harry Potter"
+end
+name
