229c846582ceab330935ce5cbc57b1e5b5d34693
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 13:37:42 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,12 +1,17 @@
+# Hey, place your code here 
 
-  puts "Hi, you've been invited to a party! What is your name?"
-  gets
+puts "Hi, you've been invited to a party! What is your name?"
+  guest_name = gets.chomp.capitalize
   party_name = "Best Halloween Party Ever"
   date = "October 31"
   time = "6pm"
   host_name = "Harry Potter"
-  guest_name = "Hermoine"
+# <<<<<<< HEAD
+  
   puts "Dear #{guest_name},
 You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
 Sincerely,
 #{host_name}"
+# =======
+
+  
